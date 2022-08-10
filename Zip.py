b424e1b1d1ca88a779cf84b1cc592c5a63245d69
2003umair <umairmohammed463@gmail.com>
2022-08-10 16:07:34 +0530

    Intermediate Program

--- Zip.py
+++ Zip.py
@@ -1,8 +1,13 @@
 i = [1, 2, 3, 4]
-j = [5, 6, 7, 8]
+j = [5, 6, 2, 1]
 k = ['a', 'b', 'c', 'd']
 
-for a,b in zip(i, j):
-    print(a, b)
+#for a,b in zip(i, j, k):
+#    print(a,b,)
+#[print(a, b, c) for a,b,c in zip(i, j, k)]
 
-print(i)    
+#[print(i, j) for i,j in zip(i, j)]
+for i,j in zip(i,j):
+    print(i,j)
+
+print(i)     
